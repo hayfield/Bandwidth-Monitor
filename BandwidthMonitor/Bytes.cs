81c93a7fa21081ef0da20c8855455f4861a53983
hayfield <hayfield@gmx.com>
2011-09-13 21:09:32 +0100

    Add a Bytes class

---
+++ BandwidthMonitor/Bytes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BandwidthMonitor
+{
+    static class Bytes
+    {
+        public static double ToKb(long bytes)
+        {
+            return (double)bytes / 1024;
+        }
+
+        public static double ToMb(long bytes)
+        {
+            return (double)bytes / 1048576;
+        }
+
+        public static double ToGb(long bytes)
+        {
+            return (double)bytes / 1073741824;
+        }
+
+        public static double ToTb(long bytes)
+        {
+            return (double)bytes / 1099511627776;
+        }
+
+        public static double ToPb(long bytes)
+        {
+            return (double)bytes / 1125899906842624;
+        }
+    }
+}
